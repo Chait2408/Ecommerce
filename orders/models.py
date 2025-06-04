f7a5b054113a36936398c9a935f345cc447eab69
Chait2408 <chaitanyajathan98@gmail.com>
2025-06-05 00:41:25 +0530

    payments

--- orders/models.py
+++ orders/models.py
@@ -63,5 +63,11 @@ class OrderProduct(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def full_name(self):
+        return f"{self.user.first_name} {self.user.last_name}"
+    
+    def full_address(self):
+        return f"{self.order.address_line_1}, {self.order.address_line_2}"
+
     def __str__(self):
         return self.product.product_name 
